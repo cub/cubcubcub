19145ce7975915d8937ee7ee8f6fe9f0739c8a03
cub <hellomynameiscub@gmail.com>
2022-08-25 17:57:18 +0200

    init cubcubcub

---
+++ src/env.d.ts
@@ -0,0 +1,3 @@
+/// <reference types="astro/client" />
+
+declare var ga: any // google-analytics
---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    fontFamily: {
+      sans: ['Ubuntu', 'sans-serif'],
+      cursive: ['Gochi Hand', 'cursive']
+    },
+    extend: {
+      dropShadow: {
+        DEFAULT: ['0 2px 0 #000', '0 4px 0 #000']
+      }
+    }
+  },
+  plugins: []
+};
