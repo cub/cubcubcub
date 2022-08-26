14a7be7d548c9ae84a8daac62d9bcec32a4dfb01
cub <hellomynameiscub@gmail.com>
2022-08-26 15:19:33 +0200

    add astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-
-/* import vue from "@astrojs/vue"; */
+import tailwind from '@astrojs/tailwind';
+import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [ tailwind()/*, vue() */]
+  integrations: [tailwind(), compress()]
 });
